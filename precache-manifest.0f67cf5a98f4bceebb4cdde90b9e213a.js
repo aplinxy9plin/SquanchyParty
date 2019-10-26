928b10c357f1239fe2760ab2e56afbccb4217dbe
AplinXy9plin <evilberry@icloud.com>
2019-10-26 12:51:31 +0300

    Updates

---
+++ precache-manifest.0f67cf5a98f4bceebb4cdde90b9e213a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00391aec6f394fa84c475813a41c69fd",
+    "url": "/ScuanchyParty/index.html"
+  },
+  {
+    "revision": "2ab8b47fcbccbe7dac53",
+    "url": "/ScuanchyParty/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "336a36ae9ee4a75dc8f0",
+    "url": "/ScuanchyParty/static/css/main.27eceecd.chunk.css"
+  },
+  {
+    "revision": "2ab8b47fcbccbe7dac53",
+    "url": "/ScuanchyParty/static/js/2.a90d079e.chunk.js"
+  },
+  {
+    "revision": "336a36ae9ee4a75dc8f0",
+    "url": "/ScuanchyParty/static/js/main.0ac4f0c7.chunk.js"
+  },
+  {
+    "revision": "e6878e140a12460eec59",
+    "url": "/ScuanchyParty/static/js/runtime-main.1b0c2e8b.js"
+  }
+]);
